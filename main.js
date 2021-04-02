9a50693cc6ade32cd222a1130e8713aee01af032
Mossakowski Kamil <mosakamil@gmail.com>
2021-04-02 18:21:59 +0200

    fixed apperance

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 
 const el_bikers = document.getElementById('bikers');
 const el_countdown = document.getElementById('countdown');
-
+const el_heart_history = document.querySelector('.heart-history');
 
 function calc_bikers_position() {
     let window_width = window.innerWidth;
@@ -11,7 +11,7 @@ function calc_bikers_position() {
     let date_now = moment();
     let date_wedding = moment("2021-08-12");
     let daysDiff = date_wedding.diff(date_now, "days");
-    if(window_width >= 576) {
+    if (window_width >= 576) {
         let position_left = (daysDiff * 100) / 422;
         el_bikers.style.left = position_left + "%";
     } else {
@@ -19,10 +19,13 @@ function calc_bikers_position() {
     }
 }
 
-setInterval(function() {
-
-let time = countdown( moment() ,moment("2021-08-12 13:00:00"), countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS ).toString();
-el_countdown.textContent = time;
+setInterval(function () {
+    let time = countdown(moment(), moment("2021-08-12 13:00:00"), countdown.DAYS | countdown.HOURS | countdown.MINUTES | countdown.SECONDS).toString();
+    el_countdown.textContent = time;
 }, 1000)
+
+el_heart_history.addEventListener('click', function() {
+    alert('elo');
+});
 
 calc_bikers_position();
