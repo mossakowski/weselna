cca793b6391a24bfd66a357497f470a36bbc9342
Mossakowski Kamil <mosakamil@gmail.com>
2021-04-02 08:01:24 +0200

    add heart history

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function calc_bikers_position() {
     let window_width = window.innerWidth;
 
     let date_now = moment();
-    let date_wedding = moment("2021-10-30");
+    let date_wedding = moment("2021-08-12");
     let daysDiff = date_wedding.diff(date_now, "days");
     if(window_width >= 576) {
         let position_left = (daysDiff * 100) / 422;
@@ -19,13 +19,10 @@ function calc_bikers_position() {
     }
 }
 
-console.log(window.innerWidth);
-
-
 setInterval(function() {
 
-let test = countdown( moment() ,moment("2021-10-30 13:00:00"), countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS ).toString();
-el_countdown.textContent = test;
+let time = countdown( moment() ,moment("2021-08-12 13:00:00"), countdown.DAYS|countdown.HOURS|countdown.MINUTES|countdown.SECONDS ).toString();
+el_countdown.textContent = time;
 }, 1000)
 
 calc_bikers_position();
