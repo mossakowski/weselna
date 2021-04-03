5624e74384523b8e56b8934326a0f331e94ae501
Mossakowski Kamil <mosakamil@gmail.com>
2021-04-03 15:48:26 +0200

    fixed heart with history and phone

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-//422 dni od zaręczyn do kościoła
-//
-
 const el_bikers = document.getElementById('bikers');
 const el_countdown = document.getElementById('countdown');
-const btn_heart_open_history = document.querySelector('.heart-open-history');
 const el_bg_history = document.querySelector('.bg-history');
+const el_text_history = document.querySelector('.block-history');
 
 const btn_close_history = document.querySelector('.btn-close-history');
+const btn_heart_open_history = document.querySelector('.heart-open-history');
 
 function calc_bikers_position() {
     let window_width = window.innerWidth;
@@ -27,14 +25,26 @@ setInterval(function () {
     el_countdown.textContent = time;
 }, 1000)
 
-btn_heart_open_history.addEventListener('click', function() {
-    // el_bg_history.style.display = "block";
-    let arr = 'align-items-center justify-items-center d-flex';
-    el_bg_history.classList.add('align-items-center', 'justify-content-center','d-flex', 'flex-column');
+btn_heart_open_history.addEventListener('click', function () {
+    let el_heart_history = document.querySelector('.fa-heart');
+    el_heart_history.classList.remove('heart-bg-close');
+    el_heart_history.classList.add('heart-bg-open');
+    el_bg_history.classList.add('align-items-center', 'justify-content-center', 'd-flex', 'flex-column');
+    el_text_history.classList.add('container-history-opacity-to-1');
+    el_text_history.classList.remove('container-history-opacity-to-0');
 });
 
-btn_close_history.addEventListener('click', function() {
-    el_bg_history.classList.remove('align-items-center', 'justify-content-center','d-flex', 'flex-column');
+btn_close_history.addEventListener('click', function () {
+    let el_heart_history = document.querySelector('.fa-heart');
+    el_heart_history.classList.remove('heart-bg-open');
+    el_heart_history.classList.add('heart-bg-close');
+    setTimeout(
+        function () {
+            el_bg_history.classList.remove('align-items-center', 'justify-content-center', 'd-flex', 'flex-column')
+        }, 800)
+    el_text_history.classList.add('container-history-opacity-to-0');
+    el_text_history.classList.remove('container-history-opacity-to-1');        
+
 })
 
 calc_bikers_position();
